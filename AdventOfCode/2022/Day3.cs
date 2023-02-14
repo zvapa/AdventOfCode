c845c19cf5cfc01b41cc86a2675629d8e74b73fc
Eduard Varlam <eduardvarlam@yahoo.com>
2023-02-14 12:03:07 +0000

    fix build

--- AdventOfCode/2022/Day3.cs
+++ AdventOfCode/2022/Day3.cs
@@ -2,5 +2,10 @@ namespace AdventOfCode._2022;
 
 public class Day3 : Puzzle
 {
+    public Day3(string inputFileName) : base(inputFileName)
+    {
+    }
 
+    public override int Solve_Part1() => throw new NotImplementedException();
+    public override int Solve_Part2() => throw new NotImplementedException();
 }
